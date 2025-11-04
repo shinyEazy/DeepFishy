ceab9d553caa56afec744b0a619c1b783ec0a765
shinyEazy <tanhngusi39@gmail.com>
2025-11-04 17:46:57 +0700

    chore: Update dependencies and clean up unused files; add langgraph_api to .gitignore

--- src/deepfishy/main.py
+++ src/deepfishy/main.py
@@ -10,7 +10,7 @@ from utils.load_agents import load_agents
 load_dotenv()
 
 
-custom_model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+# custom_model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 # custom_model = ChatOpenAI(model="gpt-4o-mini")
 subagents = load_agents(
     names=[
@@ -20,24 +20,24 @@ subagents = load_agents(
 
 # Create the agent
 agent = create_deep_agent(
-    model=custom_model,
+    model="openai:gpt-4.1-nano",
     tools=[],
     system_prompt=ORCHESTRATOR_PROMPT,
     subagents=subagents,
 )
 
-# Invoke the agent
-result = agent.invoke(
-    {
-        "messages": [
-            {
-                "role": "user",
-                "content": "Giá VN Index hiện tại là bao nhiêu",
-            }
-        ]
-    }
-)
+# # Invoke the agent
+# result = agent.invoke(
+#     {
+#         "messages": [
+#             {
+#                 "role": "user",
+#                 "content": "Giá VN Index hiện tại là bao nhiêu",
+#             }
+#         ]
+#     }
+# )
 
-output_path = os.path.join(os.getcwd(), "src/output/response.json")
-save_readable_response(result, output_path)
-print(f"Agent response written to: {output_path}")
+# output_path = os.path.join(os.getcwd(), "response.json")
+# save_readable_response(result, output_path)
+# print(f"Agent response written to: {output_path}")
